a5bd5f1dbedbafd41486051b05061aff6e2dec6b
masbarok193@gmail.com <masbarok193@gmail.com>
2022-02-21 11:07:28 +0700

    Nomor 3 Praktikum 2 Single Action Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+      public function about(){
+        return "Muhammad Rizki Mubarok 2041720001 ";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/',[PageController::class,'index']);
-Route::get('/about',[PageController::class,'about']);
-Route::get('/articles/{id}',[PageController::class,'articles']);
+Route::get('/',[HomeController::class,'index']);
+Route::get('/about',[AboutController::class,'about']);
+Route::get('/articles/{id}',[ArticleController::class,'articles']);
